fa24f4ba5af59e4cc5d13f696d6d7d51ed2da293
yu-ne <yune.dominion@gmail.com>
2021-12-08 11:54:27 +0900

    ゲーム終了条件を修正

--- setting.js
+++ setting.js
@@ -182,10 +182,7 @@ Setting.LOG_LEVEL = 1;
 Setting.isGameEnd = function() {
 
   // プレイヤー1のデッキ内の属州枚数が4枚になったらゲーム終了
-  if((board.players[0].countByCardNameInDeck(Card.NAME.PROVINCE) >= 4)
-    ||
-    (board.players[0].countByCardNameInDeck(Card.NAME.PROVINCE) >= 3
-      && board.players[0].countByCardNameInDeck(Card.NAME.DUCHY) >= 2)) {
+  if(board.players[0].countByCardNameInDeck(Card.NAME.PROVINCE) >= 4) {
     return true;
   }
 
